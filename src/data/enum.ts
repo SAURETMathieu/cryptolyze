051cbe257427cd577f4f30772e5c08fa531a78ca
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-08-16 19:05:57 +0200

    feat(data): add blockchains and exchanges enums for better data management

---
+++ src/data/enum.ts
@@ -0,0 +1,16 @@
+export const blockchains: [string, ...string[]] = [
+  'Bitcoin',
+  'Ethereum',
+  'BSC',
+  'Polygon',
+  'Solana',
+  'Avalanche',
+];
+
+export const exchanges: [string, ...string[]] = [
+  "Binance",
+  "Coinbase",
+  "Kraken",
+  "Bitstamp",
+  "Kucoin",
+];
