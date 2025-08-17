431cf94b4dbfe02b03daf6c6aa6d2c216107b0b8
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-08-17 16:08:50 +0200

    feat(api): implement GET route for fetching crypto yearly history status from Supabase

---
+++ src/app/api/crypto-histories/route.ts
@@ -0,0 +1,23 @@
+import { createApiResponse } from "@/src/lib/api/createApiResponse";
+import { withApiHandler } from "@/src/lib/api/withApiHandler";
+import { createServer } from "@/src/lib/supabase/server";
+
+export const GET = withApiHandler<any>(
+  async (req, user) => {
+    const supabase = createServer();
+    const { data, error } = await supabase
+      .from("crypto_yearly_history_status")
+      .select(`*`)
+      .order("asset", { ascending: false });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return createApiResponse(true, "crypto_histories_fetched", data, null, 200);
+  },
+  {
+    requireAuth: false,
+    defaultDataOnError: [],
+  }
+);
