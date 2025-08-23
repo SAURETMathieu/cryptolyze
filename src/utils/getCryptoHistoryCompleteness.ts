7966a0c5e95d86bbe69b26c1e33cfe19008b3a62
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-08-23 20:43:18 +0200

    refactor(utils): update getCryptoHistoryCompleteness to use string status for completeness check

--- src/utils/getCryptoHistoryCompleteness.ts
+++ src/utils/getCryptoHistoryCompleteness.ts
@@ -1,6 +1,10 @@
-export function getCryptoHistoryCompleteness(priceHistory: Record<string, boolean>) {
+export function getCryptoHistoryCompleteness(
+  priceHistory: Record<string, string>
+) {
   const entries = Object.entries(priceHistory);
   const totalYears = entries.length;
-  const completeYears = entries.filter(([_, isComplete]) => isComplete).length;
+  const completeYears = entries.filter(
+    ([_, status]) => status === "complete"
+  ).length;
   return (completeYears / totalYears) * 100;
 }
