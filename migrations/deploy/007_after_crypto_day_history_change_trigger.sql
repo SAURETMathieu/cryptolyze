cff90f09dfc18206a1768f46d8c9b1ded820c70d
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2025-11-23 14:55:16 +0100

    feat(migrations): enhance crypto_day_history trigger to include crypto information in payload

--- migrations/deploy/007_after_crypto_day_history_change_trigger.sql
+++ migrations/deploy/007_after_crypto_day_history_change_trigger.sql
@@ -11,13 +11,21 @@ SET search_path = 'public'
 AS $$
 DECLARE
     payload JSON;
+    crypto_info RECORD;
 BEGIN
+    SELECT *
+    INTO crypto_info
+    FROM crypto c
+    WHERE c.id = NEW.crypto_id;
+
     CASE TG_OP
         WHEN 'INSERT' THEN
             payload := json_build_object(
                 'event_type', 'INSERT',
                 'table', TG_TABLE_NAME,
-                'record', to_jsonb(NEW) - 'prices_per_minute',
+                'new_record', to_jsonb(NEW) - 'prices_per_minute',
+                'old_record', null,
+                'crypto', to_jsonb(crypto_info),
                 'timestamp', now()
             );
         WHEN 'UPDATE' THEN
@@ -26,13 +34,16 @@ BEGIN
                 'table', TG_TABLE_NAME,
                 'new_record', to_jsonb(NEW) - 'prices_per_minute',
                 'old_record', to_jsonb(OLD) - 'prices_per_minute',
+                'crypto', to_jsonb(crypto_info),
                 'timestamp', now()
             );
         WHEN 'DELETE' THEN
             payload := json_build_object(
                 'event_type', 'DELETE',
                 'table', TG_TABLE_NAME,
-                'record', to_jsonb(OLD) - 'prices_per_minute',
+                'new_record', null,
+                'old_record', to_jsonb(OLD) - 'prices_per_minute',
+                'crypto', to_jsonb(crypto_info),
                 'timestamp', now()
             );
     END CASE;
